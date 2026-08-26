07a9c241031ecc70624fab8b7e9def73c9cb7762
moto1092 <tokunim@gmail.com>
2026-08-26 16:07:24 +0900

    Open new batch form by default

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fermenters-ledger-v7';
+const CACHE_NAME = 'fermenters-ledger-v8';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './app-icon.svg'];
 
 self.addEventListener('install', event => {
