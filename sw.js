00007e7ff5637b04d9e4b2290ac88d3014b79c9a
moto1092 <tokunim@gmail.com>
2026-08-26 21:43:13 +0900

    Embed user guide in app

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fermenters-ledger-v19';
+const CACHE_NAME = 'fermenters-ledger-v20';
 const APP_SHELL = ['./', './index.html', './help.html', './manifest.webmanifest', './app-icon.svg', './supabase-config.js', './cloud-sync.js'];
 
 self.addEventListener('install', event => {
