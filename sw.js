8c5ab9de964d24564c948338aa05e1db4e5d557e
moto1092 <tokunim@gmail.com>
2026-08-27 11:52:58 +0900

    Improve brewing water controls and pH estimator

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fermenters-ledger-v29';
+const CACHE_NAME = 'fermenters-ledger-v30';
 const APP_SHELL = ['./', './index.html', './demo.html', './help.html', './manifest.webmanifest', './app-icon.svg', './supabase-config.js', './cloud-sync.js'];
 
 self.addEventListener('install', event => {
