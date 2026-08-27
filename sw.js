96e50a5f5cc01ab7a5f3948fc0f2e303a3ef5b25
moto1092 <tokunim@gmail.com>
2026-08-27 14:01:19 +0900

    Simplify selected batch header

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fermenters-ledger-v39';
+const CACHE_NAME = 'fermenters-ledger-v40';
 const APP_SHELL = ['./', './index.html', './demo.html', './help.html', './manifest.webmanifest', './app-icon.svg', './supabase-config.js', './cloud-sync.js'];
 
 self.addEventListener('install', event => {
